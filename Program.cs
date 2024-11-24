7d7d41786f0b6fc892c6e64a54c0780339660512
Fadydesoky <fadydesoky45@gmail.com>
2024-11-24 19:43:44 +0200

    modify
    correcting data

--- Program.cs
+++ Program.cs
@@ -6,12 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 // إضافة خدمات الـ DbContext
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddControllers();
 // إضافة الخدمات المخصصة
 builder.Services.AddScoped<StudentService>();
 builder.Services.AddScoped<AttendanceService>();
-builder.Services.AddControllers();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 // إضافة Swagger
 builder.Services.AddSwaggerGen(c =>
 {
@@ -26,12 +26,18 @@ using (var scope = app.Services.CreateScope())
     var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     AppDbContext.Seed(context); // تعبئة البيانات
 }
-
+app.UseRouting();
 // إعداد Swagger
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Student Attendance API v1"));
+    app.UseSwaggerUI();
 }
 
+#pragma warning disable ASP0014 // Suggest using top level route registrations
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+#pragma warning restore ASP0014 // Suggest using top level route registrations
 app.Run();
--- obj/Debug/net9.0/StudentAttendanceAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/StudentAttendanceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentAttendanceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01e8a1f78845e5ac4855f197dbe9cff427a4d0d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea2eb5104baeb800b9be66cfe371f55d590a75f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentAttendanceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentAttendanceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
